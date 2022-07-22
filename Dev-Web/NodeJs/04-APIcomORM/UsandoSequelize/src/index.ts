826b2f3650f12d83f65f4f1be8ec2759d58d045b
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-07-22 12:26:04 -0300

    adding cors to access control

--- Dev-Web/NodeJs/04-APIcomORM/UsandoSequelize/src/index.ts
+++ Dev-Web/NodeJs/04-APIcomORM/UsandoSequelize/src/index.ts
@@ -1,8 +1,15 @@
 import express, { Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
 import { StatusCodes } from "http-status-codes";
 
 const app = express();
 const PORT = 5000;
+const allowedOrigins = [`http://localhost:${PORT}`];
+const options: CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(options));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,5 +21,5 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
   console.log(`Application listening on port: ${PORT}`);
-  console.log(`> http://localhost:${PORT}/`);
+  console.log(`> ${[allowedOrigins]}`);
 });
